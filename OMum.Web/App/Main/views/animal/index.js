ead524449d3372dc3f3235a54ca80a94ead94a8b
daywrite@yeah.net <daywrite@yeah.net>
2015-10-27 11:13:56 +0800

    第二步：新增 1.0

--- OMum.Web/App/Main/views/animal/index.js
+++ OMum.Web/App/Main/views/animal/index.js
@@ -5,15 +5,22 @@
             var vm = this;
             vm.animalindex = {};
 
+            vm.animals = [];
+            vm.totalAnimalCount = 0;
+
             var loadAnimalIndex = function () {
                 abp.ui.setBusy(
                     null,
-                animalService.GetAnimals({
+                animalService.getAnimals({
                     MaxResultCount: 10,
                     SkipCount: 0,
                     Sorting: "Name DESC"
                 }).success(function (data) {
-                    vm.animalindex.count = data;
+                    for (var i = 0; i < data.items.length; i++) {
+                        vm.animals.push(data.items[i]);
+                    }
+
+                    vm.totalAnimalCount = data.totalCount;
                 })
                 );
             };
