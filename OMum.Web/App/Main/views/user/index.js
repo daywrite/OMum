c007067029a2f1c740f6c957a9dff050f0725ae1
daywrite@yeah.net <daywrite@yeah.net>
2015-11-09 18:03:08 +0800

    用户列表

--- OMum.Web/App/Main/views/user/index.js
+++ OMum.Web/App/Main/views/user/index.js
@@ -1,1 +1,101 @@
-﻿
+﻿(function () {
+    App.controller("UserContraller", [
+        '$scope', 'abp.services.app.user', '$modal', '$rootScope','appSession', function ($scope, userService, $modal, $rootScope,appSession) {
+            var vm = this;
+            var tenantId;
+
+            var tenant = appSession.tenant;
+
+            vm.users = [];
+            //分页基本参数
+            $scope.paginationConf = {
+                currentPage: 1,
+                itemsPerPage: 10,
+                perPageOptions: [5, 10, 15, 20]
+            };
+
+            var loadTenant = function () {
+
+                //查询参数
+                var postData = {
+                    MaxResultCount: 100,
+                    pageIndex: $scope.paginationConf.currentPage,
+                    pageSize: $scope.paginationConf.itemsPerPage,
+                    tenantId: tenant.id
+                }
+
+                abp.ui.setBusy(
+                    null,
+                    userService.getUsers(postData).success(function (data) {
+                        $scope.paginationConf.totalItems = data.totalCount;
+                        vm.users = data.items;
+                    })
+                );
+            };
+
+            $scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage', loadTenant);
+
+            //创建租户
+            vm.showNewTenant = function () {
+                var modalInstance = $modal.open({
+                    templateUrl: abp.appPath + 'App/Main/views/tenant/create.html',
+                    controller: 'app.views.tenant.create as vm',
+                    size: 'md'
+                });
+
+                modalInstance.result.then(function () {
+                    loadTenant();
+                });
+            };
+
+            //更新租户
+            vm.updateTenant = function (index) {
+
+                var scope = $rootScope.$new();
+                scope.data = vm.tenants[index];
+
+                var modalInstance = $modal.open({
+                    templateUrl: abp.appPath + 'App/Main/views/tenant/create.html',
+                    controller: 'app.views.tenant.create as vm',
+                    size: 'md',
+                    scope: scope
+                });
+
+                modalInstance.result.then(function () {
+                    loadTenant();
+                });
+            };
+
+            //删除租户
+            vm.updateTenant = function (index) {
+
+                var scope = $rootScope.$new();
+                scope.data = vm.tenants[index];
+
+                var modalInstance = $modal.open({
+                    templateUrl: abp.appPath + 'App/Main/views/tenant/create.html',
+                    controller: 'app.views.tenant.create as vm',
+                    size: 'md',
+                    scope: scope
+                });
+
+                modalInstance.result.then(function () {
+                    loadTenant();
+                });
+            };
+
+            vm.deleteTenant = function (index) {
+                var data = vm.tenants[index];
+                bootbox.setDefaults("locale", "zh_CN");
+                bootbox.confirm("确定要删除此租户？", function (r) {
+                    if (r) {
+                        tenantService.deleteTenant(data.id).success(function (data) {
+                            loadTenant();
+                        });
+                    }
+                });
+               
+            };
+        }
+    ]);
+})();
